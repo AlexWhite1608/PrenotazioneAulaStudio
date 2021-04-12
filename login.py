5630695a95dbccbd48983f4f7d1a1a4a85eb8c2f
Alessandro Bianco <alessandro.bianco1@stud.unifi.it>
2021-04-12 22:56:04 +0200

    bug fix nel login

--- login.py
+++ login.py
@@ -1,6 +1,18 @@
 import setup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def login():
+
+    try:
+        WebDriverWait(setup.driver, 5).until(
+            EC.presence_of_element_located((By.ID, "username"))
+        )
+    except:
+        print("Errore nell'apertura del sito")
+        setup.driver.quit()
+
     username = setup.driver.find_element_by_id("username")
     username.send_keys(setup.matricola)
 
