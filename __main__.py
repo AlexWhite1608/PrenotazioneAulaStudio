0a54c4ce1c4e5383debbdbb2b5eb63a57aa7d1d8
Alessandro Bianco <alessandro.bianco1@stud.unifi.it>
2021-04-12 22:49:19 +0200

    aggiunte le options per il webdriver

--- __main__.py
+++ __main__.py
@@ -4,7 +4,6 @@ import prenotazione
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from tkinter import *
 
 # apre il sito
 setup.driver.get("https://kairos.unifi.it/portalePlanning/BIBL/login.php")
--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
-
-# imposta il chromedriver
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
+from tkinter import *
 
 # ignora gli errori ssl
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--ignore-ssl-errors')
 
+# imposta il chromedriver
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH, chrome_options=options)
+
 # definisce matricola e password
 matricola = "7033449"
 password = "Alessandro1"
+
+
